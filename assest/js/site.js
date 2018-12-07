d4cd220686d3f1b1be806f5adc05f52b9370c8c5
blueshysky <xionghuih745@gmail.com>
2018-12-06 22:40:12 -0600

    added javascript with a toggle function for menu

---
+++ assest/js/site.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+var image = document.getElementById("castle");
+var button = document.getElementById("menu");
+button.addEventListener('click', function(){
+  document.getElementById("mydropdown").classList.toggle("show")
+});
+
+});
